83523d70e96cc659cc804a2e60c5c122c3e42db2
akshatsinghania <you@example.com>
2021-07-03 22:17:29 +0530

    added basic rest operations

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask,Response,jsonify
+from flask import Flask,Response,jsonify,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return Response('working')
+def index():
+    return 'working'
+
+@app.route("/",methods=['POST'])
+def sum():
+    """
+    sends back the sum of a and b provided in request json
+    """
+    response = {"sum":request.json['a']+request.json['b']}
+    return jsonify(response)
+
+@app.route("/uppercase/<string:search>",methods=['GET'])
+def uppercase(search):
+    """
+    responds with the uppercase version of the url query
+    """
+    return search.upper()
